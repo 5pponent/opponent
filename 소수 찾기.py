ce5592b877837cd6b3f70bae5ed95db36cd51ed8
opponent <ahdwjdtprtm@gmail.com>
2021-12-28 23:02:36 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42839

--- 소수 찾기.py
+++ 소수 찾기.py
@@ -1,25 +1,29 @@
+from itertools import combinations, permutations
+
 def solution(numbers):
     answer = 0
+    size = len(numbers) + 1
+    al = []; pl = []
     numbers = list(numbers)
 
-    for i in range(0, len(numbers)):
-        if isPrimeNumber(i): answer += 1; print(i)
+    for i in range(1, size):
+        al = list(map(''.join, permutations(numbers, i)))
+        al = list(set(al))
+        al = list(map(int, al))
+        print(al)
+        for j in range(0, len(al)):
+            pl.append(al[j])
 
-        for j in range(0, len(numbers)):
-            if i == j: continue
-            num = int(numbers[i]+numbers[j])
-            if isPrimeNumber(num): answer += 1; print(num)
-
-            for k in range(1, num+1):
-                n=1
-
+    pl = list(set(pl))
+    for i in range(0, len(pl)):
+        if isPrimeNumber(pl[i]):
+            answer += 1
     return answer
 
 def isPrimeNumber(n):
+    n = int(n)
     if n < 2: return False
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
-
-print(solution("011"))
