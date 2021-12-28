307ac1daa767fea36b5cb0a8ca490d6e3bd8b5b3
opponent <ahdwjdtprtm@gmail.com>
2021-12-28 16:54:13 +0900

    https://programmers.co.kr/learn/courses/30/lessons/81301

---
+++ 두 개 뽑아서 더하기.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = []
+
+    for i in range(0,len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            res = numbers[i] + numbers[j]
+
+            if res not in answer:
+                answer.append(res)
+
+    return sorted(answer)
+
+num = [2,1,3,4,1]
+print(solution(num))
+
