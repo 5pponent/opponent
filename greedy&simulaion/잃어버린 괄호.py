6be9c415e0a3bfd83bca923406c1c4fa55dfcccb
opponent <ahdwjdtprtm@gmail.com>
2022-01-24 16:28:27 +0900

    https://www.acmicpc.net/problem/14502

--- greedy&simulaion/잃어버린 괄호.py
+++ greedy&simulaion/잃어버린 괄호.py
@@ -1,6 +1,23 @@
 식 = input()
+수 = []; 연산자 = []
 
-"""
+s = 0
+for i in range(len(식)):
+    if 식[i] in ['+', '-']:
+        수.append(식[s:i])
+        연산자.append(식[i])
+        s = i + 1
+수.append(식[s:])
+
+for i in range(len(수)):
+    수[i] = str(수[i]).lstrip('0')
+
+최종식 = ''
+for i in range(len(연산자)):
+    최종식 += 수[i]
+    최종식 += 연산자[i]
+최종식 += str(수[len(수)-1])
+
 temp = list(최종식)
 idx = 0; cnt = 0
 while True:
@@ -18,7 +35,7 @@ if cnt > 0 and temp[len(temp)-1] != ')':
 
 최종식 = "".join(str(i) for i in temp)
 print(eval(최종식))
-"""
+
 """
 60-50+40-20+10
 답 : -60
